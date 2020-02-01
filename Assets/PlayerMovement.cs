7a578adc8bed9a553c8e00bd964b63498fc315e5
Morgan <mojago@protonmail.com>
2020-02-01 12:13:16 +0000

    Player Controls

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+
+    public float force = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigidBody.AddForce(transform.up * force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rigidBody.AddForce(transform.right * -force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rigidBody.AddForce(transform.up * -force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rigidBody.AddForce(transform.right * force);
+        }
+
+    }
+}
