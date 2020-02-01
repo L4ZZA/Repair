0108202019c92a3d9399fa15dc2de317e4786113
Morgan <mojago@protonmail.com>
2020-02-01 13:10:59 +0000

    Added DamageAmounts

---
+++ Assets/Scripts/Entities/EntityDamageAmounts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class EntityDamageAmounts
+{
+    public Entities.EntityType type;
+    public int damageAmount;
+}
