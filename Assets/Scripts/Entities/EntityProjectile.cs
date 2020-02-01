a435b46d80308f1932e75af633c445502aa0bfb7
Morgan <mojago@protonmail.com>
2020-02-01 16:47:33 +0000

    Create New Projectile System

--- Assets/Scripts/Entities/EntityProjectile.cs
+++ Assets/Scripts/Entities/EntityProjectile.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class EntityProjectile : MonoBehaviour
 {
-    [Header("Damage")]
+    [Header("Damage / Force")]
     [SerializeField] private int damage;
+    [SerializeField] private float force;
 
     [Header("Which layers to repair/damage?")]
     [SerializeField] private List<LayerMask> layersCanAffect = new List<LayerMask>();
 
-    [Header("Damage")]
+    [Header("Collider / Rigidbody")]
     [SerializeField] private Collider2D collider;
+    [SerializeField] private Rigidbody2D rigidBody;
+
 
 
     private void Awake()
@@ -22,15 +25,45 @@ public class EntityProjectile : MonoBehaviour
 
             if (collider == null)
             {
-                Debug.LogError(this + ": you must assign collider");
+                Debug.LogError(this + ": you must assign collider.");
+            }
+        }
+
+        if (rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+
+            if (rigidBody == null)
+            {
+                Debug.LogError(this + ": you must assign rigidBody.");
             }
         }
 
         if (layersCanAffect.Count == 0)
         {
-            Debug.LogError(this + ": you must add at least one entity type to damage");
+            Debug.LogError(this + ": you must add at least one entity type to damage.");
         }
     }
+
+
+    public void FireProjectileForward(Transform _transform)
+    {
+        if (rigidBody != null)
+        {
+            rigidBody.AddForce(_transform.forward * force);
+        }
+    }
+
+
+    public void FireProjectileAtTarget(RaycastHit2D _ray)
+    {
+        if (rigidBody != null)
+        {
+            Debug.Log("Force applied");
+            rigidBody.AddForce(_ray.point * force);
+        }
+    }
+
 
 
     public void OnCollisionEnter2D(Collision2D collision)
---
+++ Assets/Scripts/Projectiles/ProjectileController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    [Header("Projectile Prefab")]
+    [SerializeField] private GameObject projectilePrefab;
+
+    [Header("Firing Position")]
+    [SerializeField] private GameObject firingPosition;
+
+    [Header("Rotation Speed")]
+    [SerializeField] private float rotationSpeed;
+
+    public Ray rayMouse;
+    public Vector3 mousePosition;
+    public RaycastHit raycastHit;
+    public Vector3 direction;
+    public Quaternion rotation;
+
+    private void Awake()
+    {
+        if (projectilePrefab == null)
+        {
+            Debug.LogError(this + ": must added projectile.");
+        }
+
+        if (firingPosition == null)
+        {
+            Debug.LogError(this + ": must add firing position.");
+        }
+    }
+
+
+    private void RotateToMouse(GameObject _gameObject, Vector3 _destination)
+    {
+        direction = _destination - _gameObject.transform.position;
+        rotation = Quaternion.LookRotation(direction);
+        _gameObject.transform.localRotation = Quaternion.Lerp(_gameObject.transform.rotation, rotation, 1);
+    }
+
+
+    private void Rotate()
+    {
+        var mouse = Input.mousePosition;
+        var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+
+
+    private void Update()
+    {
+        Rotate();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+            SpawnProjectileAtTarget(rayHit);
+        }
+    }
+     
+
+    public void SpawnProjectileAtTarget(RaycastHit2D _rayhit)
+    {
+        if (projectilePrefab != null)
+        {
+            GameObject projectileObject = Instantiate(projectilePrefab, firingPosition.transform.position, Quaternion.identity);
+            EntityProjectile entityProjectile = projectileObject.GetComponent<EntityProjectile>();
+
+            if (entityProjectile != null)
+            {
+                //entityProjectile.FireProjectileAtTarget(_rayhit);
+                entityProjectile.FireProjectileForward(this.transform);
+            }
+
+            else Debug.Log(this + ": EntityProjectile was not found on this prefab.");
+        }
+    }
+
+
+}
