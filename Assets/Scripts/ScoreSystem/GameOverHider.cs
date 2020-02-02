6c005bb53566f88729cce52dc1e1c35c934c8ac1
Morgan <mojago@protonmail.com>
2020-02-02 14:37:48 +0000

    Before applying sprites

---
+++ Assets/Scripts/ScoreSystem/GameOverHider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverHider : MonoBehaviour
+{
+    [Header("Hide this object on GameOver?")]
+    [SerializeField] private bool hideOnGameOver;
+
+    private void OnEnable()
+    {
+        ScoreController.Action_GameOver += GameOver;
+    }
+
+
+    private void OnDisable()
+    {
+        ScoreController.Action_GameOver -= GameOver;
+    }
+
+
+    private void GameOver(string _string)
+    {
+        if (hideOnGameOver)
+        {
+            this.gameObject.SetActive(false);
+        }    
+    }
+}
