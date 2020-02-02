aa86c945fafdfd606fd2b3de6fb070efa4860343
L4ZZA <pietro.lazz@gmail.com>
2020-02-02 14:39:56 +0000

    Enemy aims correctly

---
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    public float shootingRange = 1f;
+    public float movementSpeed = 0.5f;
+
+    public bool targetAcquired = false;
+    private float dotProduct = 0;
+
+    enum eStateMachine
+    {
+        chasing,
+        aiming,
+        shooting,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetAcquired = isWithinRange();
+    }
+
+    bool isWithinRange()
+    {
+        var distanceFromTarget = target.position - transform.position;
+        Debug.Log("Distance: " + distanceFromTarget);
+        if (Mathf.Abs(distanceFromTarget.x) <= shootingRange || Mathf.Abs(distanceFromTarget.y) <= shootingRange)
+            return true;
+
+        return false;
+    }
+}
