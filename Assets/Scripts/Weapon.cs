52e0b2f4cfa8b2aed7cd9d7b47f7bb3832cf3ae0
L4ZZA <pietro.lazz@gmail.com>
2020-11-05 12:24:42 +0100

    Shooting projectiles from weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform shotPoint;
+    public float timeBetweenShots;
+
+    float shotTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool mouseLeftButtonDown = Input.GetMouseButton(0);
+        if (mouseLeftButtonDown)
+        {
+            if (Time.time > shotTime)
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                shotTime = Time.time + timeBetweenShots;
+            }
+        }
+    }
+}
