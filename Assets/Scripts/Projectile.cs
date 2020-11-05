fee0a9d39e6eff5e7862ca3d2cf08a681a55853c
L4ZZA <pietro.lazz@gmail.com>
2020-11-05 12:36:51 +0100

    Added projectile explosion and camera movement

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,16 +6,23 @@ public class Projectile : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
+    public GameObject explosion;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+        Invoke("destroyProjectile", lifeTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void destroyProjectile()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
