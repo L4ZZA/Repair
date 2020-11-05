71de4426c15ba26949b9668dea79b28fedf827cd
L4ZZA <pietro.lazz@gmail.com>
2020-11-05 11:43:22 +0100

    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+    Vector2 moveAmount;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector2 input = new Vector2(xInput, yInput);
+        moveAmount = input.normalized * speed;
+
+        bool isRunning = moveAmount != Vector2.zero;
+        if (isRunning)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+}
